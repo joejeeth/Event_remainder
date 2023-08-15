c3e25b4d57069848a1c05494085b22d1a4c3809e
joejeeth <103771326+joejeeth@users.noreply.github.com>
2023-08-15 14:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import smtplib
 import pandas
+import os
 from random import randint
 import datetime as d
 
@@ -33,8 +34,26 @@ if today in birthday_dict:
         connection.sendmail(from_addr=my_email, to_addrs=email, msg=f"Subject:Important Event\n\n"
                                                                                         f"{final_letter}")
 
+choice = input("Enter 1. Add event 0. Exit : ")
+while choice:
+    event_name = input("Enter the event name: ")
+    receiver_add = input("Enter email id of the receiver: ")
+    r_year = input("Enter the year to receive (ex: 2023 or 2024): ")
+    r_month = input("Enter the month to receive: ")
+    r_day = input("Enter the day to receive: ")
+    if os.path.exists("events1.csv") == 0:
+        file = open("events1.csv", "a")
+        file.write("name,email,year,month,day")
+        file.close()
 
+    file = open("events1.csv", "a")
+    file.write(f"\n{event_name},{receiver_add},{r_year},{r_month},{r_day}")
+    file.close()
 
+    print(f"Event name: {event_name} \nReceiver address: {receiver_add} \nDate of remainder: {r_day}-{r_month}-{r_year}"
+        f"\nYour remainder is saved successfully.")
+
+    choice = input("Enter 1. Add event 0. Exit : ")
 
 
 
